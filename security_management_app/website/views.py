4ec39fbad611454d8650a5699a7e098a125800a3
Ben Clive <bac2g10@ecs.soton.ac.uk>
2014-03-30 12:58:06 +0100

    Removed crsf token so cross domain is allowed

--- security_management_app/website/views.py
+++ security_management_app/website/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome")
 
 #Handles devices sending their software lists
+@csrf_exempt
 def device(request, device_uid):
-    return HttpResponse("Congrats " + device_uid)
+    response = HttpResponse("Hello " + device_uid)
+    response["Access-Control-Allow-Origin"] = "*"  
+    response["Access-Control-Allow-Methods"] = "POST"
+    response["Access-Control-Max-Age"] = "1000"  
+    response["Access-Control-Allow-Headers"] = "*"  
+    return response
 
