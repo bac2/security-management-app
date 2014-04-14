41eaa200e7605ba39d0bfeb767e44a91d409f81a
Ben Clive <bac2g10@ecs.soton.ac.uk>
2014-04-14 11:35:54 +0100

    Added models from munger + device model, view to update model

--- security_management_app/website/models.py
+++ security_management_app/website/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Vulnerability(models.Model):
+    cve = models.CharField(max_length=30,unique=True)
+    summary = models.CharField(max_length=1000)
+    def __unicode__(self):
+        return self.cve 
+
+class Application(models.Model):
+    cpe = models.CharField(max_length=200,unique=True)
+    vulnerability = models.ManyToManyField(Vulnerability)
+
+    def __unicode__(self):
+        return self.name
+
+class Device(models.Model):
+    owner = models.ForeignKey(User)
+    uid = models.CharField(max_length=50,unique=True)
+    os = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=50) #User can add a memorable name to a device?
+    
+    nickname.blank = True
+
+    def __unicode__(self):
+        if self.nickname is not None:
+            return self.nickname + " (" + self.uid + ")"
+        else:
+            return self.uid
+
+
--- security_management_app/website/views.py
+++ security_management_app/website/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from models import Device
+
+import json
 
 # Create your views here.
 def index(request):
@@ -10,7 +13,18 @@ def index(request):
 #Handles devices sending their software lists
 @csrf_exempt
 def device(request, device_uid):
-    response = HttpResponse("Hello " + device_uid)
+
+    if request.method == "POST":
+        json_data = json.loads(request.body)
+        device = Device.objects.get(uid=device_uid)
+        device.os = json_data['meta']['os_name']
+        device.nickname = json_data['meta']['nickname']
+        device.uid = device_uid
+        device.save()
+        
+        #Next, Munge the software list at json_data['software'] to find CPEs, etc.
+
+    response = HttpResponse()
     response["Access-Control-Allow-Origin"] = "*"  
     response["Access-Control-Allow-Methods"] = "POST"
     response["Access-Control-Max-Age"] = "1000"  
