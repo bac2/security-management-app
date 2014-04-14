4a27671fd711fd607be10fa955734614c3a8d9f6
Ben Clive <bac2g10@ecs.soton.ac.uk>
2014-04-14 20:12:32 +0100

    Attempted some munging of sent data from windows

--- security_management_app/website/views.py
+++ security_management_app/website/views.py
@@ -5,6 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from models import Device
 
 import json
+import re
 
 # Create your views here.
 def index(request):
@@ -16,15 +17,29 @@ def device(request, device_uid):
 
     if request.method == "POST":
         json_data = json.loads(request.body)
-        device = Device.objects.get(uid=device_uid)
+        try:
+            device = Device.objects.get(uid=device_uid)
+        except Device.DoesNotExist:
+            return HttpResponse("Device does not exist", status=404)
         device.os = json_data['meta']['os_name']
         device.nickname = json_data['meta']['nickname']
         device.uid = device_uid
         device.save()
         
         #Next, Munge the software list at json_data['software'] to find CPEs, etc.
+        for software in json_data['software']:
+            name = software['name'].lower()
+            
+            match = re.match("(.*?)[Vv ]*(\d+\.\d*)", name)
+            if match:
+                if software['versionString'] == "null":
+                    software['versionString'] = match.group(2)
+                name = match.group(1)
 
-    response = HttpResponse()
+        #name and software['versionString'] should be roughly correct.
+        #I can't remove the potential "Microsoft"/publishers at the start though
+                
+    response = HttpResponse(device_uid)
     response["Access-Control-Allow-Origin"] = "*"  
     response["Access-Control-Allow-Methods"] = "POST"
     response["Access-Control-Max-Age"] = "1000"  
